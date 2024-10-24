6566f84cd467178b7fbfb4641437c9b0125faa23
giuseppe <capassog97@gmail.com>
2024-10-24 11:27:32 +0200

    Added hash and signatures download

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# Summy - A Linux malware detection program 
+# Summy - A Linux malware detection program
+
 **WARNING**: the following program is not production ready.
 
 A basic malware scanner written in Rust for desktop Linux users.
 
-The application has a server that manages rules, signatures and 
+The application has a server that manages rules, signatures and
 updates and communicates with clients through UNIX sockets.
 
 ## Features
-* [ ] Signature based scanning (importing a database)
-* [ ] Rule based scanning (using sigma rules)
+
+- [ ] Signature based scanning (importing a database)
+- [ ] Rule based scanning (using sigma rules)
 
 ## Goals
-* Implement some sort of real-time protection 
-* Introduce a kernel module to inspect both network traffic and programs using eBPF
-* Implement a systray and a GUI to control the application
-* Merge different signatures and rule sources
 
-
+- Introduce a kernel module to inspect system call using eBPF
+- Implement a systray and a GUI to control the application
+- Merge different signatures and rule sources
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,22 +1,147 @@
+use std::{
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
+    os::unix::net::UnixListener,
+    path::PathBuf,
+};
+
 use clap::Parser;
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+use tempfile::tempfile;
+use zip::ZipArchive;
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+struct Cli {
+    /// Turn debugging information on
+    #[arg(short, action = clap::ArgAction::Count)]
+    verbose: u8,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(short, long, default_value = "/tmp/linux-av.sock")]
+    unix_socket: PathBuf,
+
+    #[arg(short, long, default_value = "/tmp/linux-av")]
+    config_dir: PathBuf,
 }
 
-fn main() {
-    let args = Args::parse();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
+    // You can see how many times a particular flag or argument occurred
+    // Note, only flags can have multiple occurrences
+    match cli.verbose {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    fs::create_dir_all(&cli.config_dir).unwrap();
+    let listener = UnixListener::bind(cli.unix_socket)?;
+
+    loop {
+        match listener.accept() {
+            Ok((socket, _)) => {
+                let mut request = serde_json::Deserializer::from_reader(&socket);
+                let request = Request::deserialize(&mut request).unwrap();
+                let response = handle_request(&cli.config_dir, &request);
+
+                let response: Response = if let Err(e) = response {
+                    Response {
+                        status: 1,
+                        error: Some(e),
+                    }
+                } else {
+                    Response {
+                        status: 0,
+                        error: None,
+                    }
+                };
+
+                serde_json::to_writer(socket, &response).unwrap();
+            }
+            Err(e) => println!("accept function failed: {:?}", e),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+enum Command {
+    Scan { path: PathBuf, signature_only: bool },
+    Status,
+    Update { force: bool },
+}
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    command: Command,
+}
+
+#[derive(Serialize)]
+struct Response {
+    status: u32,
+    error: Option<Error>,
+}
+
+#[derive(Serialize)]
+enum Error {
+    UnsupportedOperation { reason: String },
+    IllegalIOOperatiion,
+}
+
+fn handle_request(config_dir: &PathBuf, request: &Request) -> Result<(), Error> {
+    match &request.command {
+        Command::Scan {
+            path,
+            signature_only,
+        } => {
+            if path.is_dir() {
+                return Err(Error::UnsupportedOperation {
+                    reason: "Directory scanning not supported yet".to_string(),
+                });
+            }
+
+            let mut file = File::open(path).unwrap();
+            let mut sha256 = Sha256::new();
+            io::copy(&mut file, &mut sha256).unwrap();
+            let hash = sha256.finalize();
+            let hash = format!("{:x}", hash);
+            println!("hash: {}", hash);
+            scan_sig(config_dir, hash.as_str());
+
+            Ok(())
+        }
+        Command::Update { force } => {
+            let url = "https://bazaar.abuse.ch/export/txt/sha256/full/";
+            let mut tempfile = tempfile().unwrap();
+            let _ = reqwest::blocking::get(url)
+                .unwrap()
+                .copy_to(&mut tempfile)
+                .unwrap();
+
+            ZipArchive::new(tempfile)
+                .unwrap()
+                .extract(config_dir)
+                .unwrap();
+
+            Ok(())
+        }
+        Command::Status => Ok(()),
+    }
+}
+
+fn scan_sig(config_dir: &PathBuf, hash: &str) {
+    let file = File::open(config_dir.join("full_sha256.txt")).unwrap();
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        if let Ok(a) = line {
+            if a == hash {
+                println!("found");
+                break;
+            }
+        }
     }
 }
