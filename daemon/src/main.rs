def7acc49ef8fd153ebd7380d001fa9a94b2cc21
giuseppe <capassog97@gmail.com>
2024-10-24 23:26:43 +0200

    Refactoring

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -45,50 +45,71 @@ mod daemon {
     const MB_SHA256_FILE: &str = "full_sha256.txt";
     const VERSION: &str = "v0.1-alpha";
 
+    const YR_CORE_URL: &str =
+        "https://github.com/YARAHQ/yara-forge/releases/latest/download/yara-forge-rules-core.zip";
+
+    const YR_CORE_FILE: [&'static str; 3] = ["packages", "core", "yara-rules-core.yar"];
     use std::{
         fs::File,
         io::{self, BufRead, BufReader, Cursor},
         os::unix::net::UnixListener,
         path::PathBuf,
+        sync::Arc,
     };
 
     use serde::{Deserialize, Serialize};
     use sha2::{Digest, Sha256};
     use tempfile::tempfile;
+    use yara::Compiler;
     use zip::ZipArchive;
 
     #[derive(Serialize, Deserialize)]
     enum Command {
-        Scan { path: PathBuf, signature_only: bool },
+        Scan { path: PathBuf, offline: bool },
         Status,
         Update,
+        NoOp,
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Deserialize)]
     pub struct Request {
         command: Command,
     }
 
-    #[derive(Serialize, Deserialize)]
-    pub struct Response {
-        status: u32,
-        result: Option<ScanResult>,
+    #[derive(Serialize)]
+    pub struct UpdateResult {
+        status: i32,
     }
 
-    #[derive(Serialize, Deserialize)]
+    #[derive(Serialize)]
     pub struct ScanResult {
         threat: bool,
-        intelligence: Option<Vec<YaraResult>>,
+        intelligence: Option<Vec<String>>,
     }
 
-    #[derive(Serialize, Deserialize)]
-    pub struct YaraResult {
-        name: String,
-        url: String,
-        description: String,
+    #[derive(Serialize)]
+    pub struct StatusResult {
+        version: String,
+        rtp_on: bool,
     }
+
+    #[derive(Serialize)]
+    pub enum CommandResult {
+        Scan(ScanResult),
+        Update(UpdateResult),
+        Status(StatusResult),
+        NoOp,
+    }
+
     #[derive(Debug)]
-    pub struct Error {}
+    pub enum Error {
+        NetworkError { err: reqwest::Error },
+        IoError,
+        ZipError,
+        YaraError { reason: String },
+
+        InvalidRequest(serde_json::Error),
+    }
 
     #[derive(Debug)]
     pub struct Daemon {
@@ -109,15 +130,22 @@ mod daemon {
             }
         }
 
-        pub async fn start(self: &Self) -> Result<(), Box<dyn std::error::Error>> {
+        pub async fn start(self: Self) -> Result<(), Box<dyn std::error::Error>> {
             let listener = UnixListener::bind(self.unix_path.clone())?;
+            let me = Arc::new(self);
 
             loop {
                 match listener.accept() {
                     Ok((socket, _)) => {
                         let mut request = serde_json::Deserializer::from_reader(&socket);
-                        let request = Request::deserialize(&mut request).unwrap();
-                        let response = self.handle_request(&request).await.unwrap();
+                        // TODO: handle invalid requests
+                        let request = Request::deserialize(&mut request).unwrap_or_else(|e| {
+                            println!("request error: {:?}", e);
+                            Request {
+                                command: Command::NoOp,
+                            }
+                        });
+                        let response = me.clone().handle_request(&request).await.unwrap();
 
                         serde_json::to_writer(socket, &response).unwrap();
                     }
@@ -125,72 +153,127 @@ mod daemon {
                 }
             }
         }
-        pub async fn handle_request(self: &Self, request: &Request) -> Result<Response, Error> {
+
+        pub async fn handle_request(
+            self: Arc<Self>,
+            request: &Request,
+        ) -> Result<CommandResult, Error> {
+            let me = self.clone();
             match &request.command {
-                Command::Scan {
-                    path,
-                    signature_only,
-                } => {
+                Command::Scan { path, offline } => {
                     if path.is_dir() {
                         todo!();
                     }
 
-                    let mut file = File::open(path).unwrap();
-                    let mut sha256 = Sha256::new();
-                    io::copy(&mut file, &mut sha256).unwrap();
-                    let hash = sha256.finalize();
-                    let hash = format!("{:x}", hash);
+                    let path1 = path.clone();
+                    let t1 = tokio::spawn(async {
+                        let mut file = File::open(path1).unwrap();
+                        let mut sha256 = Sha256::new();
+                        io::copy(&mut file, &mut sha256).map_err(|_| Error::IoError)?;
+                        let hash = sha256.finalize();
+                        let hash = format!("{:x}", hash);
 
-                    let is_threat = self.scan_sig(hash.as_str());
+                        me.scan_sig(hash.as_str())
+                    });
 
-                    if !signature_only {
-                        todo!();
-                    }
+                    let path = path.clone();
+                    let t2 = tokio::spawn(async move { self.yara_scan(path) });
 
-                    Ok(Response {
-                        status: 0,
-                        result: Some(ScanResult {
-                            threat: is_threat,
-                            intelligence: None,
-                        }),
-                    })
+                    let (r1, r2) = tokio::join!(t1, t2);
+                    let is_threat = r1.unwrap()?;
+
+                    let yr_result = r2.unwrap()?;
+
+                    Ok(CommandResult::Scan(ScanResult {
+                        threat: is_threat,
+                        intelligence: Some(yr_result),
+                    }))
                 }
                 Command::Update => {
-                    let mut tempfile = tempfile().unwrap();
-                    let response = reqwest::get(MB_SHA256_URL).await.unwrap();
-                    let mut stream = Cursor::new(response.bytes().await.unwrap());
+                    let t1 =
+                        tokio::spawn(download_and_extract(MB_SHA256_URL, me.config_path.clone()));
+                    let t2 =
+                        tokio::spawn(download_and_extract(YR_CORE_URL, me.config_path.clone()));
 
-                    io::copy(&mut stream, &mut tempfile);
+                    let (r1, r2) = tokio::join!(t1, t2);
+                    //TODO: handle results
 
-                    ZipArchive::new(tempfile)
-                        .unwrap()
-                        .extract(self.config_path.clone())
-                        .unwrap();
-
-                    Ok(Response {
-                        status: 0,
-                        result: None,
-                    })
+                    Ok(CommandResult::Update(UpdateResult { status: 0 }))
                 }
-                Command::Status => Ok(Response {
-                    status: 0,
-                    result: None,
-                }),
+                Command::Status => Ok(CommandResult::Status(StatusResult {
+                    rtp_on: self.rtp_on.clone(),
+                    version: self.version.clone(),
+                })),
+                Command::NoOp => Ok(CommandResult::NoOp),
             }
         }
-        fn scan_sig(self: &Self, hash: &str) -> bool {
-            let file = File::open(self.config_path.clone().join(MB_SHA256_FILE)).unwrap();
+        fn scan_sig(self: Arc<Self>, hash: &str) -> Result<bool, Error> {
+            let file = File::open(self.config_path.clone().join(MB_SHA256_FILE))
+                .map_err(|_| Error::IoError)?;
 
             let reader = BufReader::new(file);
 
             for line in reader.lines() {
                 if let Ok(a) = line {
                     if a == hash {
-                        return true;
+                        return Ok(true);
                     }
                 }
             }
-            return false;
+            Ok(false)
         }
+        fn yara_scan(self: Arc<Self>, target: PathBuf) -> Result<Vec<String>, Error> {
+            let yr_path = self
+                .config_path
+                .join(YR_CORE_FILE[0])
+                .join(YR_CORE_FILE[1])
+                .join(YR_CORE_FILE[2]);
+
+            println!("{:?}", yr_path.to_str());
+
+            let compiler = Compiler::new()
+                .map_err(|e| Error::YaraError {
+                    reason: e.to_string(),
+                })?
+                .add_rules_file(yr_path)
+                .map_err(|e| Error::YaraError {
+                    reason: e.to_string(),
+                })?;
+
+            let rules = compiler.compile_rules().map_err(|e| Error::YaraError {
+                reason: e.to_string(),
+            })?;
+            let result = rules
+                .scan_file(target, 60)
+                .map_err(|e| Error::YaraError {
+                    reason: e.to_string(),
+                })?
+                .iter()
+                .map(|r| r.identifier.to_string())
+                .collect::<Vec<String>>();
+
+            Ok(result)
+        }
+    }
+
+    async fn download_and_extract(url: &str, dest_directory: PathBuf) -> Result<(), Error> {
+        let mut tempfile = tempfile().map_err(|_| Error::IoError)?;
+        let response = reqwest::get(url)
+            .await
+            .map_err(|err| Error::NetworkError { err })?
+            .bytes()
+            .await
+            .map_err(|err| Error::NetworkError { err })?;
+
+        let mut stream = Cursor::new(response);
+
+        io::copy(&mut stream, &mut tempfile);
+
+        ZipArchive::new(tempfile)
+            .map_err(|_| Error::ZipError)?
+            .extract(dest_directory)
+            .map_err(|_| Error::ZipError)?;
+
+        Ok(())
     }
 }
